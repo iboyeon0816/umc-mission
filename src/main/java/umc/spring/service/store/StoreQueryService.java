e8f50b3f51a54c73ca26424923fdd4fcafd6a4e4
iboyeon0816 <iboyeon0816@gmail.com>
2023-12-12 20:22:28 +0900

    [FEAT] 가게 미션 목록 조회 API 구현

--- src/main/java/umc/spring/service/store/StoreQueryService.java
+++ src/main/java/umc/spring/service/store/StoreQueryService.java
@@ -1,6 +1,7 @@
 package umc.spring.service.store;
 
 import org.springframework.data.domain.Page;
+import umc.spring.domain.Mission;
 import umc.spring.domain.Review;
 
 public interface StoreQueryService {
@@ -8,4 +9,6 @@ public interface StoreQueryService {
     boolean exists(Long id);
 
     Page<Review> getReviews(Long storeId, Integer page);
+
+    Page<Mission> getMissions(Long storeId, Integer page);
 }
--- src/main/java/umc/spring/service/store/StoreQueryServiceImpl.java
+++ src/main/java/umc/spring/service/store/StoreQueryServiceImpl.java
@@ -5,8 +5,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import umc.spring.domain.Mission;
 import umc.spring.domain.Review;
 import umc.spring.domain.Store;
+import umc.spring.repository.MissionRepository;
 import umc.spring.repository.ReviewRepository;
 import umc.spring.repository.StoreRepository;
 
@@ -17,6 +19,9 @@ public class StoreQueryServiceImpl implements StoreQueryService {
 
     private final StoreRepository storeRepository;
     private final ReviewRepository reviewRepository;
+    private final MissionRepository missionRepository;
+
+    private static final int DEFAULT_SIZE = 10;
 
     @Override
     public boolean exists(Long id) {
@@ -26,7 +31,13 @@ public class StoreQueryServiceImpl implements StoreQueryService {
     @Override
     public Page<Review> getReviews(Long storeId, Integer page) {
         Store store = storeRepository.getReferenceById(storeId);
-        return reviewRepository.findAllByStore(store, PageRequest.of(page, 10));
+        return reviewRepository.findAllByStore(store, PageRequest.of(page, DEFAULT_SIZE));
+    }
+
+    @Override
+    public Page<Mission> getMissions(Long storeId, Integer page) {
+        Store store = storeRepository.getReferenceById(storeId);
+        return missionRepository.findAllByStore(store, PageRequest.of(page, DEFAULT_SIZE));
     }
 
 }
