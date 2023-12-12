918397e45c15a587ac0a007862c1b965d8f99ce3
iboyeon0816 <iboyeon0816@gmail.com>
2023-12-12 23:16:42 +0900

    [FEAT] 진행 상태에 따른 멤버 미션 목록 조회 API 구현

--- src/main/java/umc/spring/service/member/MemberQueryService.java
+++ src/main/java/umc/spring/service/member/MemberQueryService.java
@@ -2,8 +2,12 @@ package umc.spring.service.member;
 
 import org.springframework.data.domain.Page;
 import umc.spring.domain.Review;
+import umc.spring.domain.enums.MissionStatus;
+import umc.spring.domain.mapping.AssignedMission;
 
 public interface MemberQueryService {
 
     Page<Review> getReviews(Long memberId, Integer page);
+
+    Page<AssignedMission> getMissions(Long memberId, MissionStatus status, Integer page);
 }
--- src/main/java/umc/spring/web/dto/MissionResponseDTO.java
+++ src/main/java/umc/spring/web/dto/MissionResponseDTO.java
@@ -45,4 +45,30 @@ public class MissionResponseDTO {
         private LocalDate deadline;
         private LocalDate createdAt;
     }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class AMDetailListDTO {
+        private List<AMDetailDTO> AMDetailDTOs;
+        private Integer listSize;
+        private Boolean isFirst;
+        private Boolean isLast;
+        private Integer totalPages;
+        private Long totalElements;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class AMDetailDTO {
+        private Boolean success;
+        private Integer price;
+        private Integer point;
+        private PointType pointType;
+        private LocalDate deadline;
+        private LocalDate createdAt;
+    }
 }
